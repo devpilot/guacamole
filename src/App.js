e08050f12b70e9144bc2c948d701625019d443ab
Aritra <aet419@gmail.com>
2021-09-03 22:27:00 +0530

    removed logging

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import AddTask from './components/AddTask';
 import Tasks from './components/Tasks';
-import Logger from './components/Logger';
 
 const App = () => {
   return (
-    <>
     <div className="container">
       <h1 className="header">Todo App</h1>
       <AddTask />
       <Tasks />
     </div>
-    <div className="container">
-      <Logger />
-    </div>
-    </>
   );
 }
 
